f4096ece39aedc84022114061c1075fc142bdc57
Shadreck kasera <shadkasera@gmail.com>
2023-04-06 17:27:20 +0200

    updated files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tobacco_app/homescreen.dart';
+
+import 'package:tobacco_app/pages/welcome.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
      
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen()
+      home: const WelcomePage()
     );
   }
 }
--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:tobacco_app/pages/homescreen.dart';
 
-class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({super.key});
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 25,
+                ),
+                child: Text(
+                  'TOBACCO FARMERS REGISTRATION',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Text(
+                  'Promotng and developing the tobacco industry in Malawi and advance the interest of tobacco growers',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              //picture of the app
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: ClipOval(
+                  child: Image.asset(
+                    'assets/images/tobacco0.png',
+                    height: 300,
+                  ),
+                ),
+              ),
+              //email text field
+              const SizedBox(
+                height: 40,
+              ),
+
+              //password text field
+
+              //sign in
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomeScreen(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'GET STARTED',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+
+              //register
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
